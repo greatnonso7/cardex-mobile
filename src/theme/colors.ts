fb041f38a78cec5b22bcea04e217540b1dec4ee6
greatnonso7 <greatchinonso7@gmail.com>
2023-08-03 20:33:52 +0100

    Set up custom components and fonts

---
+++ src/theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  WHITE: '#FFFFFF',
+  PRIMARY: '#47EAA6',
+  BLACK: '#000000',
+  LIGHT_PRIMARY: '#47EAA633',
+  OFF_PRIMARY: '#EDF6F5',
+};
---
+++ src/theme/font.ts
@@ -0,0 +1,8 @@
+export const font = {
+  LexendThin: 'Lexend-Thin',
+  LexendBlack: 'Lexend-Black',
+  LexendBold: 'Lexend-Bold',
+  LexendMedium: 'Lexend-Medium',
+  LexendRegular: 'Lexend-Regular',
+  LexendSemiBold: 'Lexend-SemiBold',
+};
